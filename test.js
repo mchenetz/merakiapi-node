578fc677688c70855628342a3bc2bd15e1b21d3f
mike@chenetz.net <mike@chenetz.net>
2016-06-13 20:46:00 -0400

    Added env variables for testing

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-var merakiapi = require('./merakiapi.js')
+var merakiapi = require('./merakiapi.js');
+//Change below values to actual apikey and orgid.
+// example:
+// apikey = '35635643643563634634'
+// orgid = 12345'
 
-var org = merakiapi.getOrgDevices('Apikey','Org', (data) => {
+apikey = process.env.apikey;
+orgid = process.env.orgid;
+
+
+merakiapi.getOrgDevices(apikey,orgid, (data) => {
     for(i=0;i < data.length; i++){
             console.log(data[i]['mac'])
     }
