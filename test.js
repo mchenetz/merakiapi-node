b693affcabcd0aaa8bba943ca8f1ca686265daf7
Michael Chenetz <mike@chenetz.net>
2016-06-22 17:35:10 -0400

    merakiapi.js: added _putPath, _delPath method merakiapi.js: added addNetwork method
    test.js: added test method for addNetwork

--- test.js
+++ test.js
@@ -9,20 +9,20 @@ orgid = process.env.orgid;
 
 //The below function will return your orgid.
 
-merakiapi.getOrg(apikey, (data) => {
-    for(i=0;i < data.length; i++){
-            console.log(data[i])
-    }
-    //console.log(data);
-});
-
-//Example below gets the Meraki network by the name, 'Home' and then calls the inner function, 'getNetworkDevices'.
-//The network id is passed through the response object.
-merakiapi.getNetworkByName(apikey,orgid,'Home', (response) => {
-    merakiapi.getNetworkDevices(apikey,response, (data) => {
-        console.log(data);
-    });
-});
+// merakiapi.getOrg(apikey, (data) => {
+//     for(i=0;i < data.length; i++){
+//             console.log(data[i])
+//     }
+//     //console.log(data);
+// });
+//
+// //Example below gets the Meraki network by the name, 'Home' and then calls the inner function, 'getNetworkDevices'.
+// //The network id is passed through the response object.
+// merakiapi.getNetworkByName(apikey,orgid,'Home', (response) => {
+//     merakiapi.getNetworkDevices(apikey,response, (data) => {
+//         console.log(data);
+//     });
+// });
 
 //Example below gets the Meraki network by the name, 'Home' and then calls the inner function, 'addVlan'
 //the format of addVlan is apikey, networkid, vlanid, vlanname, mxid, subnet
@@ -33,4 +33,11 @@ merakiapi.getNetworkByName(apikey,orgid,'Home', (response) => {
 //     });
 // });
 
+// merakiapi.getNetworkList(apikey,orgid,(response) => {
+//     console.log(response);
+// });
+
+merakiapi.addNetwork(apikey,orgid, 'testnet2', 'wireless', 'test', 'America/New_York', (response) => {
+    console.log(response);
+});
 
